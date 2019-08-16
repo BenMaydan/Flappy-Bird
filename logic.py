c2b11b08864d2e998b6a9f92c1f7aeaf08bfdbcd
Ben Maydan <maydanb@gmail.com>
2019-08-16 16:40:10 -0700

    Pipe can go off of the screen now.

--- logic.py
+++ logic.py
@@ -24,45 +24,102 @@ def increasing(values, amount):
     return True
 
 
+class ScoreEngine:
+    """
+    A blueprint class for detecting if the bird is in between the pipes, granting one score
+
+    Possibly use map / filter / reduce for checking if bird is in the pipe
+    """
+
+    def __init__(self, increment=1):
+        assert increment > 0, "The score needs to increment by more than 0 every time the bird goes through a pipe!"
+        self._score = 0
+        self.increment = increment
+
+    def score(self):
+        return self._score
+
+    def increase_score(self):
+        """
+        Increments self.score by self.increment
+        :return: self.score
+        """
+        self._score += self.increment
+        return self.score()
+
+
 class CollisionEngine:
     """
     A blueprint for checking collision between the bird and the pipes currently on the screen
+
+    Possibly use map / filter / reduce for checking collision
+    Or write these methods in cython to increase speed slightly
     """
+
     @staticmethod
-    def border_collision(game):
+    def border_collision(score_engine, bird):
         """
         Checks for collision between the bird and the border
         :return: True or False
         """
-        assert isinstance(game, Game)
-        for coordinate in game.bird.coordinates:
+        assert isinstance(bird, Bird), "Bird needs to be an instance of the bird class to access it's title and name!"
+        for coordinate in bird.coordinates:
             # Touching the top of the screen
             if coordinate[0] < 0:
-                print("GAME OVER!\n{} {} touched the top of the screen!".format(game.bird.title, game.bird.name))
+                print("{} {} touched the top of the screen!\nYour score was {}".format(bird.title, bird.name,
+                                                                                       score_engine.score()))
                 sys.exit()
             # Touching the bottom of the screen
             if coordinate[0] == curses.LINES:
-                print("GAME OVER!\n{} {} touched the bottom of the screen!".format(game.bird.title, game.bird.name))
+                print("GAME OVER!\n{} {} touched the bottom of the screen!\nYour score was {}".format(bird.title,
+                                                                                                      bird.name,
+                                                                                                      score_engine.score()))
                 sys.exit()
 
     @staticmethod
-    def pipe_collision(game):
+    def pipe_collision(score_engine, bird, pipes):
         """
         Checks for collision between the bird and a pipe
-        :return:
+        :return: None
         """
-        assert isinstance(game, Game)
-        for pipe in game.pipes:
-            for coordinate in game.bird.coordinates:
+        assert isinstance(bird, Bird)
+        for pipe in pipes:
+            for coordinate in bird.coordinates:
                 if coordinate in pipe.coordinates:
-                    print("GAME OVER!\n{} {} touched a pipe!".format(game.bird.title, game.bird.name))
+                    print(
+                        "{} {} touched a pipe!\nYour score was: {}".format(bird.title, bird.name, score_engine.score()))
                     sys.exit()
+
+    @staticmethod
+    def between_pipe(score_engine, bird, pipes):
+        """
+        Checks if the bird is between a pipe
+        :param score_engine: An instance of the ScoreEngine class
+        :param bird: An instance of the Bird class
+        :param pipes: A list of all of the pipes on the screen
+        :return: True or False
+        """
+        for pipe in pipes:
+            top_left = bird.coordinates[0]
+            bottom_right = bird.coordinates[-1]
+            # If the top left coordinate of the bird is in the opening of the pipe
+            if (top_left[0] < pipe.top or top_left[0] > pipe.bottom) and (
+                    top_left[1] > pipe.coordinates[0][1] and top_left < pipe.coordinates[-1][1]):
+                score_engine.increase_score()
+                return True
+            # If the bottom right coordinate of the bird is in the opening of the pipe
+            elif (bottom_right[0] < pipe.top or bottom_right[0] > pipe.bottom) and (
+                    bottom_right[1] > pipe.coordinates[0][1] and bottom_right < pipe.coordinates[-1][1]):
+                score_engine.increase_score()
+                return True
+        return False
 
 
 class Bird:
     """
     Handles flapping and gravity
     """
+
     def __init__(self, title='Mr.', name='Glappy Glird', char='#'):
         self.char = char
         # Size + coordinates
@@ -70,7 +127,7 @@ class Bird:
         self.width = None
         self.x = None
         self.y = None
-        self.coordinates = set()
+        self.coordinates = []
 
         # Used for printing when the game ends
         self.title = title
@@ -91,7 +148,7 @@ class Bird:
         self.y = y
         for y_coord in range(y, y + height):
             for x_coord in range(x, x + width):
-                self.coordinates.add((y_coord, x_coord))
+                self.coordinates.append((y_coord, x_coord))
         return self.coordinates
 
     def flap(self, game, amount):
@@ -104,7 +161,7 @@ class Bird:
         # Deletes the bird on the screen before redrawing
         game.long_del(self.coordinates)
         # Adjusts every coordinate that makes up the bird to move up some amount
-        self.coordinates = list(map(lambda coordinate: (coordinate[0] - amount,  coordinate[1]), self.coordinates))
+        self.coordinates = list(map(lambda coordinate: (coordinate[0] - amount, coordinate[1]), self.coordinates))
         return self.coordinates
 
     def fall(self, game, amount):
@@ -120,15 +177,25 @@ class Bird:
         self.coordinates = list(map(lambda coordinate: (coordinate[0] + amount, coordinate[1]), self.coordinates))
         return self.coordinates
 
+    def coast(self):
+        """
+        Does not fall and does not flap. AKA does nothing
+        :return: None
+        """
+        pass
+
 
 class Pipe:
     """
     A blueprint for creating pipes randomly
     """
+
     def __init__(self, char='&', yrange=None):
         self.char = char
 
         # Boilerplate code for a method call later to build self
+        self.top = None
+        self.bottom = None
         self.xrange = None
         self.yrange = (0, curses.LINES)
         self.coordinates = []
@@ -145,7 +212,8 @@ class Pipe:
         left = width[0]  # Readability
         right = width[1]  # Readability
         assert (left > -1), "The minimum x coordinate of the screen is 0 and you gave {}".format(left)
-        assert (right <= curses.COLS - 1), "The maximum x coordinate of the screen is {} and you gave {}".format(curses.COLS - 1, right)
+        assert (right <= curses.COLS - 1), "The maximum x coordinate of the screen is {} and you gave {}".format(
+            curses.COLS - 1, right)
         assert (top >= 0), "The minimum y coordinate of the screen is 0 and you gave {} for the top of the opening of " \
                            "the pipe".format(top)
         assert (bottom <= curses.LINES), "The maximum y coordinate of the screen is {} and you gave {} for the bottom " \
@@ -153,6 +221,8 @@ class Pipe:
         assert (top < bottom), "Overlapping ends of the opening of the pipe. Top: {}, Bottom: {}\nRemember, " \
                                "the x axis in curses starts at the top of the screen and the y coordinate increments " \
                                "by 1 as it goes down".format(top, bottom)
+        self.top = top
+        self.bottom = bottom
         self.xrange = width
 
         for y in range(self.yrange[0], self.yrange[1]):
@@ -173,8 +243,13 @@ class Pipe:
         :param x: A range of x coordinates of the columns of the pipe to delete
         :return: The new self.coordinates
         """
-        assert x[0] == 0, "The first number in the range of x coordinates of columns to delete from the pipe needs to " \
-                          "be 0 "
+        # If the range of x values is empty
+        if len(x) == 0:
+            return
+
+        # Else delete columns of the pipe like normal
+        assert x[0] == 0, "The first number in the range of x coordinates of columns to delete from the pipe needs" \
+                          " to be 0 "
         assert increasing(x, 1), "The range of x coordinates of columns to delete from the pipe needs to increase by 1"
 
         # Converts x to a list so we can change the values
@@ -193,8 +268,14 @@ class Pipe:
         :param amount: The amount to move forward or backwards
         :return: The new coordinates
         """
-        # Deletes the pipe on the screen before redrawing
-        game.long_del(self.coordinates)
+        # Deletes the pipe on the screen before redrawing. If parts of the pipe are off the screen, it is ignored
+        for coordinate in self.coordinates:
+            try:
+                game.stdscr.addstr(coordinate[0], coordinate[1], " ")
+            except Exception:
+                # Ignore if pipe is slightly off of the screen, that will be handled in tick
+                pass
+
         # Adjusts every coordinate that makes up the bird to move up some amount
         self.coordinates = list(map(lambda coordinate: (coordinate[0], coordinate[1] - amount), self.coordinates))
         return self.coordinates
@@ -204,6 +285,7 @@ class Game:
     """
     A class for drawing with python curses
     """
+
     def __init__(self, bird=Bird(char='#'), sleep=0.1):
         # Holds the "sprites" currently on the screen
         self.pipes = []
@@ -216,7 +298,7 @@ class Game:
         self.sleep = sleep
 
         # This score will be printed at the end. It is incremented every time the snake eats a piece of food
-        self.score = 0
+        self.ScoreEngine = ScoreEngine(increment=1)
         self.frozen = False
 
     def __enter__(self):
@@ -265,40 +347,45 @@ class Game:
         Performs a tick. If the up arrow is not pressed, the bird starts to fall down
         :return: None
         """
-        # Moves all of the pipes backwards before allowing any input
-        add = self.stdscr.addstr
         for pipe in self.pipes[:]:
-            # If the pipe doesn't have coordinates (off of the screen) the except block is triggered
-            try:
-                if pipe.coordinates[0][1] < 0:
-                    # Delete the first three columns of the pipe
-                    pipe.delete(0, 1, 2)
-                # Keep moving the pipe
-                for coord in pipe.move(self, 3):
-                    try:
-                        add(coord[0], coord[1], pipe.char)
-                    except Exception as e:
-                        # If the left of the pipe is starting to go off of the screen
-                        pass
-            except IndexError:
-                # The pipe is off of the screen, deleting it is ok
+            # Move the pipe backwards
+            pipe.move(self, 3)
+
+            # Draw the new pipe. If there is a curses error, delete that column
+            columns_to_delete = set()
+            current_column = 0
+            for coord in pipe.coordinates:
+                try:
+                    self.stdscr.addstr(coord[0], coord[1], pipe.char)
+                except Exception as e:
+                    pass
+
+            # If the pipe is completely off of the screen, delete the pipe
+            if pipe.coordinates[-1][1] < 1:
                 self.pipes.remove(pipe)
-                # Pipe can be garbage collected
                 del pipe
         self.refresh()
 
-        # Takes input from the user
+        # Acts off of user input
         inp = self.getch()
-        # 119 = W key
-        # curses.KEY_UP = up arrow
-        if inp == 119 or inp == curses.KEY_UP:
+        if inp == 119 or inp == curses.KEY_UP:  # 119 = W key
             self.bird.flap(self, 3)
+            # Reset tick value to 0 so bird does not coast fall coast fall etc...
+            self.tick_value = 0
         else:
-            self.bird.fall(self, 2)
+            # Wait one tick before falling
+            if self.tick_value == 0:
+                self.bird.coast()
+                # Don't want the bird to coast forever
+                self.tick_value += 1
+            else:
+                self.bird.fall(self, 2)
+                self.tick_value += 1
 
-        # Checks for collision with the border before updating the coordinates of the bird on the screen
-        CollisionEngine.border_collision(self)
-        CollisionEngine.pipe_collision(self)
+        # Checks for collision before updating the coordinates of the bird on the screen
+        CollisionEngine.border_collision(self.ScoreEngine, self.bird)
+        CollisionEngine.pipe_collision(self.ScoreEngine, self.bird, self.pipes)
+        CollisionEngine.between_pipe(self.ScoreEngine, self.bird, self.pipes)
 
         # If the game did not end because of a collision, the bird's position is updated
         self.long_add(self.bird.char, self.bird.coordinates)
